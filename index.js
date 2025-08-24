537aa7c48c46050fa349cbf86ad8b21ada449c66
Y. DUSHYANTH <yalamanchilidushyanth@gmail.com>
2025-08-25 02:59:04 +0530

    changed

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 import leadRoutes from './routes/leadRoutes.js';
 import connectDB from './utils/dbConnect.js';
+import cors from 'cors';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
